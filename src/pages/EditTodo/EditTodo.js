f2e04c31c62bd816236077e12abe7fb2645c0b0d
Elisei Miron <elisei.miron@rebeldot.com>
2023-05-23 17:40:48 +0300

    struggling with Edit Todo item...

--- src/pages/EditTodo/EditTodo.js
+++ src/pages/EditTodo/EditTodo.js
@@ -8,8 +8,6 @@ import { PARAMS } from "routes";
 export default function EditTodo() {
   const { [PARAMS.TODO_ID]: todoId } = useParams();
 
-  console.log({ todoId });
-
   const {
     data: { todos },
     errors,
@@ -17,19 +15,54 @@ export default function EditTodo() {
     isLoading,
   } = useGetUsersAndTodos();
 
+  function findTodoById(id) {
+    const todoData = todos.find((todo) => todo.data === id);
+    return todoData;
+  }
+  console.log(findTodoById(todoId));
+
   const renderError = () => {
     return (
       <div>
         Error
         {errors.forEach((error) => {
-          if (error) return <>{error.status}</>;
+          if (error) return error.status;
         })}
       </div>
     );
   };
 
+  const renderChangeCurrentTodo = () => {
+    return (
+      <>
+        <p>Current todo item: {todoId}</p>
+        <p>Current todo item: {findTodoById(todoId)}</p>
+
+        {todos.map((todo) => {
+          return (
+            <>
+              <p>{todo.data}</p>
+            </>
+          );
+        })}
+
+        <div>
+          <input type="text" />
+          <button>Update</button>
+
+          {typeof postSuccess === "boolean" &&
+            (postSuccess ? (
+              <p>Post added successfully</p>
+            ) : (
+              <p>Something went wrong</p>
+            ))}
+        </div>
+      </>
+    );
+  };
+
   const renderTodo = () => {
-    return <>{isError ? renderError() : <p>Check: TODO ID: {todoId}</p>}</>;
+    return isError ? renderError() : renderChangeCurrentTodo();
   };
 
   return isLoading ? <Loader /> : renderTodo();
